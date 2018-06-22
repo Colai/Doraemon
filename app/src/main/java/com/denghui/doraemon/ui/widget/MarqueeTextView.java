c0151bbdafdb27f1169050250ea0c4b6b103937b
denghui <denghui@mafengwo.com>
2018-06-22 11:04:53 +0800

    【控件】跑马灯TextView，手动这是焦点状态，保证跑马灯效果生效

---
+++ app/src/main/java/com/denghui/doraemon/ui/widget/MarqueeTextView.java
@@ -0,0 +1,53 @@
+package com.denghui.doraemon.ui.widget;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+
+public class MarqueeTextView extends android.support.v7.widget.AppCompatTextView {
+    private boolean isMarqueeEnable = false;
+
+    public MarqueeTextView(Context context) {
+        super(context);
+    }
+
+    public MarqueeTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MarqueeTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public void setMarqueeEnable(boolean enable) {
+        if (isMarqueeEnable != enable) {
+            isMarqueeEnable = enable;
+            if (enable) {
+                setEllipsize(TextUtils.TruncateAt.MARQUEE);
+            } else {
+                setEllipsize(TextUtils.TruncateAt.END);
+            }
+            onWindowFocusChanged(enable);
+        }
+    }
+
+    public boolean isMarqueeEnable() {
+        return isMarqueeEnable;
+    }
+
+    @Override
+    public boolean isFocused() {
+        return isMarqueeEnable;
+    }
+
+    @Override
+    protected void onFocusChanged(boolean focused, int direction, Rect previouslyFocusedRect) {
+        super.onFocusChanged(isMarqueeEnable, direction, previouslyFocusedRect);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasWindowFocus) {
+        super.onWindowFocusChanged(isMarqueeEnable);
+    }
+}
